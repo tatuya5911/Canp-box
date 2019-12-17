b60090d7aefb122e46dd493f5c3fec5f7df12968
tatuya <kkb76904@gmail.com>
2019-12-17 15:19:13 +0900

    [finish]3-9

---
+++ sample_app3-9/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'e68e016d33efbb135a1ea58b3319f77bdf080c0fce64d1224b0773f95d65a79a5eeed0f8778a54d083e5ad21a24c884ca741a2fd9890c64adfd938a500eb5111'
