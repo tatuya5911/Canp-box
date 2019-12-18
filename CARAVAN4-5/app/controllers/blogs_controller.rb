617b719b0f8f730bf9f3a22ac409a61910358765
tatuya <kkb76904@gmail.com>
2019-12-18 13:24:43 +0900

    [finish]4-5

---
+++ CARAVAN4-5/app/controllers/blogs_controller.rb
@@ -0,0 +1,31 @@
+class BlogsController < ApplicationController
+  def index
+    @blogs = Blog.all
+  end
+
+  def show
+   @blog = Blog.find(params[:id])
+  end
+
+  def new
+  	@blog=Blog.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def create
+        blog = Blog.new(blog_params)
+        blog.save
+        redirect_to blogs_path
+  end
+
+  private
+  def blog_params
+      params.require(:blog).permit(:title, :category, :body)
+  end
+
+end
